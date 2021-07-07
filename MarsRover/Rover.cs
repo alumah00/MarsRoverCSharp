ff199befb96a34c5e67aca9f137d28fb964991f0
alumah00 <chrishamula@gmail.com>
2021-07-07 09:44:43 -0400

    Still not done with 13 but 1-12 looks done-ish

--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -9,6 +9,34 @@ namespace MarsRover
 
         public Rover(int position)
         {
+            Position = position;
+            Mode = "NORMAL";
+            GeneratorWatts = 110;
+        }
+
+        public void ReceiveMessage(Message message)
+        {
+            for (int i = 0; i < message.Commands.Length; i++)
+            {
+                string commandType = message.Commands[i].CommandType;
+
+                if (commandType == "MODE_CHANGE")
+                {
+                    Mode = message.Commands[i].NewMode;
+                }
+                else if (commandType == "MOVE")
+                {
+                    if (Mode != "LOW_POWER")
+                    {
+                        Position = message.Commands[i].NewPosition;
+                    }
+                }
+                else if (commandType != "MODE_CHANGE" || commandType != "MOVE")
+                {
+                    throw new ArgumentException("This Command Type is denied");
+                }
+            }
+
         }
 
         public override string ToString()
--- MarsRoverTests/RoverTests.cs
+++ MarsRoverTests/RoverTests.cs
@@ -8,6 +8,79 @@ namespace MarsRoverTests
     [TestClass]
     public class RoverTests
     {
+        //8
+        [TestMethod]
+        public void ConstructorSetsDefaultPosition()
+        {
+            Rover newPosition = new Rover(100);
+            Assert.AreEqual(newPosition.Position, 100);
+        }
+        //9
+        [TestMethod]
+        public void ConstructorSetsDefaultMode()
+        {
+            Rover newMode = new Rover(100);
+            Assert.AreEqual(newMode.Mode, "NORMAL");
+        }
+        //10
+        [TestMethod]
+        public void ConstructorSetsDefaultGeneratorWatts()
+        {
+            Rover newGW = new Rover(110);
+            Assert.AreEqual(newGW.GeneratorWatts, 110);
+        }
+        //11
+        [TestMethod]
+        public void RespondsCorrectlyToModeChangeCommand()
+        {
+            Command[] commands = { new Command("MODE_CHANGE", 500, "LOW_POWER") };
+            Message newMessage= new Message("HAPPY", commands);
+            Rover newChange = new Rover(250);
 
+            newChange.ReceiveMessage(newMessage);
+
+            string expectedRoverMode = "LOW_POWER";
+            string actualRoverMode = newChange.Mode;
+
+            Assert.AreEqual(expectedRoverMode, actualRoverMode);
+        }
+
+
+        //12
+        [TestMethod]
+        public void DoesNotMoveInLowPower()
+        {
+            Command[] commands = { new Command("MODE_CHANGE", 500, "LOW_POWER"), new Command("MOVE", 10) };
+        
+            Message newMessage = new Message("HAPPY", commands);
+            Rover newChange = new Rover(250);
+
+            newChange.ReceiveMessage(newMessage);
+
+            string expectedRoverMode = "LOW_POWER";
+            string actualRoverMode = newChange.Mode;
+
+            Assert.AreEqual(expectedRoverMode, actualRoverMode);
+
+            int expectedRoverPosition = 250;
+            int actualRoverPosition = newChange.Position;
+
+            Assert.AreEqual(expectedRoverPosition, actualRoverPosition);
+        }
+        //13  Not Done!! Just copied from 12 without mods yet
+        [TestMethod]
+        public void PositionChangesFromMoveCommand()
+        {
+            Command[] commands = { new Command("MODE_CHANGE", 500, "LOW_POWER") };
+            Message newMessage = new Message("HAPPY", commands);
+            Rover newChange = new Rover(250);
+
+            newChange.ReceiveMessage(newMessage);
+
+            string expectedRoverMode = "LOW_POWER";
+            string actualRoverMode = newChange.Mode;
+
+            Assert.AreEqual(expectedRoverMode, actualRoverMode);
+        }
     }
 }
